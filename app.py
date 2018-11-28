26ee610c49722383e2071f13f7e0c32e0c13f5df
gary917 <gary991177@gmail.com>
2018-11-28 22:10:48 +1100

    dynamically displays availability zone

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request
+import boto3
+import random
+import uuid
+
 application = Flask(__name__)
+ec2_client = boto3.client('ec2')
+response = ec2_client.describe_availability_zones()
 
 
 @application.route('/')
 def index():
-	print("request received!!")
-	return render_template('index.html')
+	zone = response['AvailabilityZones'][0]['ZoneName']
+	return render_template('index.html', availability_zone = zone)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', threaded=True)
